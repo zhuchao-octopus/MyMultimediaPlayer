a0d6951fd79c2b0b9492cb7f5a045951f512f755
zrh <trenail@qq.com>
2024-04-20 08:27:41 +0800

    feature ui 收音机页面基础布局

---
+++ app/src/main/java/com/octopus/android/multimedia/fragments/fm/FmFragment.kt
@@ -0,0 +1,8 @@
+package com.octopus.android.multimedia.fragments.fm
+
+import com.octopus.android.multimedia.R
+import com.octopus.android.multimedia.fragments.BaseFragment
+
+class FmFragment : BaseFragment(R.layout.fragment_fm) {
+
+}
