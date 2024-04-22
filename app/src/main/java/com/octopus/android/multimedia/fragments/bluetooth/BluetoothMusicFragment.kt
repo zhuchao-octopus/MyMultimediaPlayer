6aa985d64004f3ba6a3cbe3232a3ffd3ee92b656
zrh <trenail@qq.com>
2024-04-22 15:07:01 +0800

    feature ui 蓝牙音乐播放页面

--- app/src/main/java/com/octopus/android/multimedia/fragments/bluetooth/BluetoothMusicFragment.kt
+++ app/src/main/java/com/octopus/android/multimedia/fragments/bluetooth/BluetoothMusicFragment.kt
@@ -1,6 +1,20 @@
 package com.octopus.android.multimedia.fragments.bluetooth
 
+import android.os.Bundle
+import android.view.View
+import com.octopus.android.multimedia.R
+import com.octopus.android.multimedia.databinding.FragmentBluetoothMusicBinding
 import com.octopus.android.multimedia.fragments.BaseFragment
+import com.octopus.android.multimedia.utils.setOnClickListenerWithInterval
+import com.octopus.android.multimedia.utils.viewBinding
 
-class BluetoothMusicFragment : BaseFragment() {
+class BluetoothMusicFragment : BaseFragment(R.layout.fragment_bluetooth_music) {
+    private val binding: FragmentBluetoothMusicBinding by viewBinding()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.viewPrev.setOnClickListenerWithInterval { }
+        binding.viewNext.setOnClickListenerWithInterval { }
+        binding.viewPlay.setOnClickListenerWithInterval { }
+    }
 }
--- app/src/main/java/com/octopus/android/multimedia/fragments/bluetooth/BluetoothPairFragment.kt
+++ app/src/main/java/com/octopus/android/multimedia/fragments/bluetooth/BluetoothPairFragment.kt
@@ -1,6 +1,7 @@
 package com.octopus.android.multimedia.fragments.bluetooth
 
+import com.octopus.android.multimedia.R
 import com.octopus.android.multimedia.fragments.BaseFragment
 
-class BluetoothPairFragment : BaseFragment() {
+class BluetoothPairFragment : BaseFragment(R.layout.fragment_bluetooth_pair) {
 }
