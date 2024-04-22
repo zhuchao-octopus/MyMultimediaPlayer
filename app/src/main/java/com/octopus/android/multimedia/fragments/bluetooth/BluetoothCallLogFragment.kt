e997174e1b746cbaef2fd940351cd1bced1588e4
zrh <trenail@qq.com>
2024-04-22 15:15:28 +0800

    feature ui 蓝牙通话记录页面

--- app/src/main/java/com/octopus/android/multimedia/fragments/bluetooth/BluetoothCallLogFragment.kt
+++ app/src/main/java/com/octopus/android/multimedia/fragments/bluetooth/BluetoothCallLogFragment.kt
@@ -1,6 +1,7 @@
 package com.octopus.android.multimedia.fragments.bluetooth
 
+import com.octopus.android.multimedia.R
 import com.octopus.android.multimedia.fragments.BaseFragment
 
-class BluetoothCallLogFragment : BaseFragment() {
+class BluetoothCallLogFragment : BaseFragment(R.layout.fragment_bluetooth_call_log) {
 }
