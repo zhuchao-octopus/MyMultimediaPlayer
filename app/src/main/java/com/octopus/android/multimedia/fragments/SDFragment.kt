7ae8c9e001a6b9dc1a8009f928bb52c25c21eb1f
zrh <trenail@qq.com>
2024-04-23 09:28:44 +0800

    feature ui 支持显示sd卡视频

--- app/src/main/java/com/octopus/android/multimedia/fragments/SDFragment.kt
+++ app/src/main/java/com/octopus/android/multimedia/fragments/SDFragment.kt
@@ -1,6 +1,11 @@
 package com.octopus.android.multimedia.fragments
 
 import com.airbnb.mvrx.fragmentViewModel
+import com.zhuchao.android.fbase.MMLog
+import com.zhuchao.android.fbase.MessageEvent
+import com.zhuchao.android.fbase.MethodThreadMode
+import com.zhuchao.android.fbase.TCourierSubscribe
+import com.zhuchao.android.fbase.eventinterface.EventCourierInterface
 import com.zhuchao.android.session.Cabinet
 import com.zhuchao.android.video.OMedia
 import kotlinx.coroutines.delay
@@ -8,15 +13,24 @@ import kotlinx.coroutines.delay
 //sd卡视频列表页面
 class SDFragment : VideoListFragment() {
     override val viewModel: SDViewModel by fragmentViewModel()
+
+    @TCourierSubscribe(threadMode = MethodThreadMode.threadMode.MAIN)
+    fun onTCourierSubscribeEvent(courierInterface: EventCourierInterface) {
+        MMLog.d(tag, "onTCourierSubscribeEvent:$courierInterface")
+        when (courierInterface.id) {
+            //媒体库更新后,刷新列表页面
+            MessageEvent.MESSAGE_EVENT_SD_VIDEO -> {
+                viewModel.fetchData()
+            }
+        }
+    }
+
 }
 
 class SDViewModel(initialState: VideoListState) : VideoListViewModel(initialState) {
     override fun fetchData() {
         suspend {
-            //TODO 获取SD卡视频列表数据
-            //模拟加载耗时场景
-            //delay(5000)
-            //listOf("1", "2", "3")
+            //获取SD卡视频列表数据
             Cabinet.getPlayManager().localSDMediaVideos.all.values.toList() as List<OMedia>
         }.execute { copy(list = it) }
     }
